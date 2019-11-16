3d33d9df961301d2f99ec9dfeebdf1700aa6f0ee
mata02 <55011633+mata02@users.noreply.github.com>
2019-11-16 13:10:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-<script>
-$("body").waitMe({});
-
-window.addEventListener("load",function(){
-  $("body").waitMe("hide");.
-},false);
-</script>
+document.addEventListener('DOMContentLoaded', function(event) {
+    console.log("DOM fully loaded and parsed");
+}, false);
+window.addEventListener('load', function(event) {
+    console.log("All resources finished loading!");
+}, false);
+/*
+* inlineスタイルはそのまま
+* toggle設定はhideWaitMe()の直前（ctpは削除）
+* Globalize.culture設定はそのままイベント登録しない
+* メニューコンテンツはctp内で取得（Controller内での設定は削除）
+* ログインユーザ名は<h5 class="dropdown-header">Dropdown header</h5>
+* メッセージ定義ファイルの同期確認
+*/
