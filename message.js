d93b6b28a2eed1d875880b1a72456460136d2d55
mata02 <55011633+mata02@users.noreply.github.com>
2019-12-07 12:49:45 +0900

    Update message.js

--- message.js
+++ message.js
@@ -1,5 +1,12 @@
-$('#container').waitMe();
-$('.modal-body').load('/not-here.php', 'modal', function(response, status, xhr) {
-  $('#addnewWindow').modal('show');
-  $('#container').waitMe("hide");
+$('#select').on('change', function() {
+  $('#container').waitMe();
+  $('.modal-master').load('/not-here.php body', 'modal', function(response, status, xhr) {
+    if (status === 'success') {
+      $('#addnewWindow').modal('show');
+      $('#container').waitMe("hide");
+    } else {
+      $('#container').waitMe("hide");
+      ajaxFail(xhr.statusText, 'E', '通信エラーが発生しました。');
+    }
+  });
 });
