ab91d38d4a018cc7a419c17ca95242fadbfa34a5
mata02 <55011633+mata02@users.noreply.github.com>
2019-12-07 12:55:22 +0900

    Update message.js

--- message.js
+++ message.js
@@ -1,6 +1,8 @@
+メッセージ域を<div class="modal-body">内に定義する
+
 $('#select').on('change', function() {
   $('#container').waitMe();
-  $('.modal-master').load('/not-here.php body', 'modal', function(response, status, xhr) {
+  $('.modal-master').load('/not/here body', 'modal', function(response, status, xhr) {
     if (status === 'success') {
       $('#addnewWindow').modal('show');
       $('#container').waitMe("hide");
