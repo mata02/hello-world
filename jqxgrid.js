c60ebca34668044cda04fa39d91244b7fb25afd8
mata02 <55011633+mata02@users.noreply.github.com>
2019-12-27 21:42:19 +0900

    Update jqxgrid.js

--- jqxgrid.js
+++ jqxgrid.js
@@ -1,10 +1,12 @@
-function getRowIndex(id) {
-    var rows = $('#jqxgrid').jqxGrid('getdisplayrows');
+
+function selectedRowById(selector, id) {
+    var rows = $(selector).jqxGrid('getdisplayrows');
     for (var i = 0; i < rows.length; i++) {
         var row = rows[i];
         if (row.id === id) {
-          return row.boundindex;
+            $(selector).jqxGrid({ selectedrowindex: row.boundindex});
+            return;
         }
     }
-    return null;
+    return;
 }
