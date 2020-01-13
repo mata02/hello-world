3454e26aceceab5a4f809472151ea373e0c34668
mata02 <55011633+mata02@users.noreply.github.com>
2020-01-13 22:29:09 +0900

    Update table.php

--- table.php
+++ table.php
@@ -1,13 +1,13 @@
 $query = <<<SQL
-WITH RECURSIVE anc (class,name,code,parent) AS (
- SELECT permission.class,permission.name,permission.code,permission.parent
+WITH RECURSIVE an (level,name,code,parent) AS (
+ SELECT permission.level,permission.name,permission.code,permission.parent
    FROM permission
-  WHERE permission.code = :controller
+  WHERE permission.code = :controller AND permission.lang = 'ja_JP'
  UNION ALL
- SELECT permission.class,permission.name,permission.code,permission.parent
-   FROM anc,permission
-  WHERE anc.parent = permission.code)
-SELECT name FROM anc ORDER BY class;
+ SELECT permission.level,permission.name,permission.code,permission.parent
+   FROM an,permission
+  WHERE an.parent = permission.code AND an.lang = 'ja_JP')
+SELECT name FROM an ORDER BY level;
 SQL;
 
 $results = $connection->execute(query, ['controller' => 'AppController'])
