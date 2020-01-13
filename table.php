55006a7c011664a01190260d7d0ebfe34341da3e
mata02 <55011633+mata02@users.noreply.github.com>
2020-01-13 22:34:02 +0900

    Update table.php

--- table.php
+++ table.php
@@ -1,12 +1,12 @@
 $query = <<<SQL
-WITH RECURSIVE an (level,name,code,parent) AS (
- SELECT permission.level,permission.name,permission.code,permission.parent
+WITH RECURSIVE an (level,name,code,parent,lang) AS (
+ SELECT permission.level,permission.name,permission.code,permission.parent,permission.lang
    FROM permission
   WHERE permission.code = :controller AND permission.lang = 'ja_JP'
  UNION ALL
- SELECT permission.level,permission.name,permission.code,permission.parent
+ SELECT permission.level,permission.name,permission.code,permission.parent,permission.lang
    FROM an,permission
-  WHERE an.parent = permission.code AND an.lang = 'ja_JP')
+  WHERE an.parent = permission.code AND an.lang = permission.lang)
 SELECT name FROM an ORDER BY level;
 SQL;
 
