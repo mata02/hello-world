5e3b2b4adc2a973b11771e20aa1d60630709e454
mata02 <55011633+mata02@users.noreply.github.com>
2020-02-09 10:06:57 +0900

    Update checked.js

--- checked.js
+++ checked.js
@@ -7,22 +7,15 @@
 // 所属区分
 // 所属区分名：所属区分マスタ内でユニーク
 $(function() {
- 
-  // ボタンをクリックしたら発動
   $('#btn').click(function() {
- 
     // もしチェックが入っていたら
     if ($('input[name="check"]').prop('checked')) {
- 
       // チェックを外す
       $('input[name="check"]').prop('checked', false);
- 
     // もしチェックが外れていたら
     } else {
- 
       // チェックを入れる
       $('input[name="check"]').prop('checked', true);
     }
- 
   });
 });
