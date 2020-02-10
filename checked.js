40c24f8565af9663b43e8c47840e0cee84e1c105
mata02 <55011633+mata02@users.noreply.github.com>
2020-02-10 22:26:35 +0900

    Update checked.js

--- checked.js
+++ checked.js
@@ -1,10 +1,3 @@
-// ログイン
-// * 利用規約デフォルト値ON
-// * 利用規約prop設定変更
-// 高さ計算
-// * outerHeight(true)
-// splitter幅指定
-// * 99.9%
 // ユーザマスタ
 // * 作業者略名：作業者マスタの同一所属区分内でユニーク
 // 作業者マスタ
@@ -13,16 +6,3 @@
 // * ユーザー->システム利用者
 // 所属区分
 // * 所属区分名：所属区分マスタ内でユニーク
-$(function() {
-  $('#btn').click(function() {
-    // もしチェックが入っていたら
-    if ($('input[name="check"]').prop('checked')) {
-      // チェックを外す
-      $('input[name="check"]').prop('checked', false);
-    // もしチェックが外れていたら
-    } else {
-      // チェックを入れる
-      $('input[name="check"]').prop('checked', true);
-    }
-  });
-});
