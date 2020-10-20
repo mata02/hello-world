de077aa5620254877b9a08a46cf720fe7738ae4f
mata02 <55011633+mata02@users.noreply.github.com>
2020-10-20 21:15:46 +0900

    Create isOnline.js

---
+++ isOnline.js
@@ -0,0 +1,12 @@
+const isOnline = async () => {
+  const date = new Date();
+  const timestamp = date.getTime();
+
+  try {
+    await fetch(`/ysap/favicon.ico?${timestamp}`);
+  } catch {
+    return false;
+  }
+
+  return true;
+};
