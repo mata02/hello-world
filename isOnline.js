222564b1de64ea1748261768aa537e2dc0935b8f
mata02 <55011633+mata02@users.noreply.github.com>
2020-12-29 22:40:27 +0900

    Update isOnline.js

--- isOnline.js
+++ isOnline.js
@@ -1,3 +1,13 @@
+export default {
+  build: {
+    extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-cheap-module-source-map'
+      }
+    }
+  }
+}
+
 async function isOnline() {
   const date = new Date();
   const timestamp = date.getTime();
