4870c11b7f312e58742b2e0194a963b12e934104
mata02 <55011633+mata02@users.noreply.github.com>
2021-01-26 22:02:14 +0900

    Update isOnline.js

--- isOnline.js
+++ isOnline.js
@@ -1,18 +1,3 @@
-var result = (function (param1, param2) {
-    return param1 + param2
-}(1, 2))
-console.log(result)
-
-const locale = (async function (lang) {
-  if (lang !== 'en') {
-    const { jqx } =  await import(`../../assets/locales/jqx.${lang}`)
-    return jqx
-  } else {
-    return {}
-  }
-}(process.env.APP_LANG))
-console.log(locale)
-
 async function isOnline() {
   const date = new Date();
   const timestamp = date.getTime();
