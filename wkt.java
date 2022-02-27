f79c88c6499646d86eb3675815c20a63d6dc7fc7
mata02 <55011633+mata02@users.noreply.github.com>
2022-02-27 14:18:57 +0900

    Delete wkt.java

--- wkt.java
+++
@@ -1,8 +0,0 @@
-11     /** The SRID regular expression. */
-12     public static final String SRID_REGEX = "SRID[\\s]*=[\\s]*([0-9]+)[\\s]*;";
-13  
-14     /** The WKT regular expression. */
-15     public static final String WKT_REGEX = "((((MULTI)?(POINT|LINESTRING|POLYGON)|GEOMETRYCOLLECTION)Z?M?)[\\s]*[(].*[)])";
-16  
-17     /** The PostGIS EWKT regular expression. */
-18     public static final String EWKT_REGEX = "(" + SRID_REGEX + "[\\s]*)?" + WKT_REGEX;
