8fa64c3502e4cdfcb25b1521ce7cb1364c7f4d32
mata02 <55011633+mata02@users.noreply.github.com>
2022-06-25 12:26:29 +0900

    Update sidebar.vue

--- sidebar.vue
+++ sidebar.vue
@@ -4,7 +4,7 @@
     <b-sidebar id="sidebar-footer" sidebar-class="border-left border-danger" shadow="sm" no-header right>
       <template #footer="{ hide }">
        <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
-         <b-button size="sm" variant="secondary" @click="hide" style="position: absolute;display:block;top:50vh;left:-37px">
+         <b-button size="sm" variant="secondary" @click="hide" style="position: absolute;top:50vh;left:-36px">
            <b-icon icon="arrow-bar-right"></b-icon>
          </b-button>
          <strong class="mr-auto">Footer</strong>
