c4579247e52b9169881e4e31cff48ddf4ca4353f
mata02 <55011633+mata02@users.noreply.github.com>
2022-08-13 11:56:01 +0900

    Update setval.sql

--- setval.sql
+++ setval.sql
@@ -1,1 +1,11 @@
 SELECT setval(pg_get_serial_sequence('t1', 'id'), coalesce(max(id),0)+1, false) FROM t1;
+
+// テーブルの存在確認
+if (Schema::hasTable('bk1_mobile_app_packages')) {
+    // "bk1_mobile_app_packages"テーブルは存在していた
+}
+
+// カラムのリネーム
+Schema::table('mobile_app_packages', function (Blueprint $table) {
+    $table->renameColumn('app_type', 'app_type_id');
+});
