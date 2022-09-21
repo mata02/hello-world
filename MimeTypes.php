77b300553352d31d109238b4dc5d651e67958dc2
mata02 <55011633+mata02@users.noreply.github.com>
2022-09-21 22:25:16 +0900

    Create MimeTypes.php

---
+++ MimeTypes.php
@@ -0,0 +1,5 @@
+private const REVERSE_MAP = [
+  'text' => ['text/plain'],
+  'csv' => ['text/csv', 'application/csv', 'text/x-comma-separated-values', 'text/x-csv'],
+  'zip' => ['application/zip', 'application/x-zip', 'application/x-zip-compressed'],
+];
