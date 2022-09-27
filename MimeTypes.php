90b605e01b77576237c43b3d985ecb247c1cbcc8
mata02 <55011633+mata02@users.noreply.github.com>
2022-09-27 20:55:04 +0900

    Update MimeTypes.php

--- MimeTypes.php
+++ MimeTypes.php
@@ -4,11 +4,3 @@ private const REVERSE_MAP = [
   'zip' => ['application/zip', 'application/x-zip', 'application/x-zip-compressed'],
 ];
 
-<div>
-  <b-alert show variant="light">
-    <b-row>
-      <b-col cols="12" md="6">こんにちは&nbsp;<span class="font-weight-bold">鈴木一郎</span>&nbsp;さん</b-col>
-      <b-col cols="12" md="6" class="pl-md-0 text-md-right">前回ログイン日時: 2022-09-22 19:08</b-col>
-    </b-row>
-  </b-alert>
-</div>
