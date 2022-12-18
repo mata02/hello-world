df4fc06126ec0b61a4ee9f436f9ff2ff54422289
mata02 <55011633+mata02@users.noreply.github.com>
2022-12-18 21:29:25 +0900

    Delete MimeTypes.php

--- MimeTypes.php
+++
@@ -1,6 +0,0 @@
-private const REVERSE_MAP = [
-  'text' => ['text/plain'],
-  'csv' => ['text/csv', 'application/csv', 'text/x-comma-separated-values', 'text/x-csv'],
-  'zip' => ['application/zip', 'application/x-zip', 'application/x-zip-compressed'],
-];
-
