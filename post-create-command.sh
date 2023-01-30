be11904b4280bb16a9c97031218b64e75b6610b4
mata02 <55011633+mata02@users.noreply.github.com>
2023-01-30 22:39:56 +0900

    Update post-create-command.sh

--- post-create-command.sh
+++ post-create-command.sh
@@ -1,6 +1,8 @@
+https://code.visualstudio.com/docs/remote/troubleshooting#_ssh-tips
+https://code.visualstudio.com/docs/remote/troubleshooting#_setting-up-the-ssh-agent
+
 "postCreateCommand": "./.devcontainer/post-create-command.sh",
 
-post-create-command.sh
 
 apt-get update && \
 apt-get install -y git openssh-client
