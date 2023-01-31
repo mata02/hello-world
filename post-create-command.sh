6e23e37c17c1bab88e29ef214af3f42c00371dd4
mata02 <55011633+mata02@users.noreply.github.com>
2023-01-31 20:54:34 +0900

    Delete post-create-command.sh

--- post-create-command.sh
+++
@@ -1,8 +0,0 @@
-https://code.visualstudio.com/docs/remote/troubleshooting#_ssh-tips
-https://code.visualstudio.com/docs/remote/troubleshooting#_setting-up-the-ssh-agent
-
-"postCreateCommand": "./.devcontainer/post-create-command.sh",
-
-
-apt-get update && \
-apt-get install -y git openssh-client
