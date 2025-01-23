860d160a93a00a414bee3474133f3b308628725f
mata02 <55011633+mata02@users.noreply.github.com>
2025-01-23 22:47:21 +0900

    Delete backup_restore.sh

--- backup_restore.sh
+++
@@ -1,2 +0,0 @@
-sudo docker compose exec ${dockerのホスト名} pg_dumpall --clean --username ${user名} > /tmp/${ファイル名}.sql
-sudo cat /tmp/${ファイル名}.sql | docker-compose exec -T ${dockerのホスト名} psql --username ${user名}
