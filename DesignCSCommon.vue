4b1b02a1272ba8cabe75538ea502deac30d8b609
mata02 <55011633+mata02@users.noreply.github.com>
2026-02-07 19:01:04 +0900

    Update DesignCSCommon.vue

--- DesignCSCommon.vue
+++ DesignCSCommon.vue
@@ -1,3 +1,4 @@
+packages\themes\src\presets\lara\togglebutton\index.ts
 export const root: ToggleButtonTokenSections.Root = {
     checkedBackground: '{highlight.focusBackground}',
     checkedColor: '{highlight.focusColor}',
@@ -7,3 +8,41 @@ export const icon: ToggleButtonTokenSections.Icon = {
     checkedColor: '{highlight.focusColor}',
 
 };
+
+packages\themes\src\presets\lara\datatable\index.ts
+export const headerCell: DataTableTokenSections.HeaderCell = {
+    selectedBackground: '{highlight.focusBackground}',
+
+    selectedColor: '{highlight.focusColor}',
+};
+export const row: DataTableTokenSections.Row = {
+
+    selectedBackground: '{highlight.focusBackground}',
+
+    selectedColor: '{highlight.focusColor}',
+};
+
+packages\themes\src\presets\lara\treetable\index.ts
+export const headerCell: TreeTableTokenSections.HeaderCell = {
+    selectedBackground: '{highlight.focusBackground}',
+
+    selectedColor: '{highlight.focusColor}',
+};
+export const row: TreeTableTokenSections.Row = {
+
+    selectedBackground: '{highlight.focusBackground}',
+
+    selectedColor: '{highlight.focusColor}',
+};
+
+packages\themes\src\presets\lara\tree\index.ts
+export const node: TreeTokenSections.Node = {
+
+    selectedBackground: '{highlight.focusBackground}',
+
+    selectedColor: '{highlight.focusColor}',
+};
+
+packages\themes\src\presets\lara\datepicker\index.ts
+packages\themes\src\presets\lara\organizationchart\index.ts
+packages\themes\src\presets\lara\paginator\index.ts
