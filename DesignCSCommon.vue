3416e6982ef9c6abe574f4a8fadcc3ca3d667e8e
mata02 <55011633+mata02@users.noreply.github.com>
2026-02-07 18:38:11 +0900

    Create DesignCSCommon.vue

---
+++ DesignCSCommon.vue
@@ -0,0 +1,9 @@
+export const root: ToggleButtonTokenSections.Root = {
+    checkedBackground: '{highlight.focusBackground}',
+    checkedColor: '{highlight.focusColor}',
+
+};
+export const icon: ToggleButtonTokenSections.Icon = {
+    checkedColor: '{highlight.focusColor}',
+
+};
